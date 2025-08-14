360c6b429e782bb00b2721ad51f4b6cefa98f5bb
Kartik <kartik3344@gmail.com>
2025-08-14 18:46:56 +0530

    added mailer function

---
+++ index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors"
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('connected to Db')
+        app.listen(PORT, () => {
+            console.log('Served at port 3000')
+        })
+    })
+    .catch((err) => console.error('mongodb error', err))
