970b00e5913c31dcd2a0bc9195bc0e1269b63970
Kartik <kartik3344@gmail.com>
2025-08-14 19:23:00 +0530

    init inngest and created inngest pipeline for user signup

---
+++ inngest/client.js
@@ -0,0 +1,3 @@
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({ id: 'ticket-iq' }); //name of the collection of functions for this app
---
+++ inngest/functions/on-signup.js
@@ -0,0 +1,37 @@
+import { inngest } from "../client";
+import User from '../../models/user'
+import { NonRetriableError } from "inngest";
+import { sendMail } from "../../utils/mailer";
+
+export const onUserSignUp = inngest.createFunction(
+    { id: "om-user-signup", retries:5 },
+    { event: "user/signupp" },
+
+    async ({ event,step }) => {
+        try {
+            const {email} = event.data;
+            const user = await step.run('get-user-email', async() => {
+                const userObject = await User.findOne({ email });
+                if(!userObject){
+                    throw new NonRetriableError("User does not exist in Db")
+                }
+                return userObject;
+            })
+            
+            await step.run("send-welcome-email", async () => {
+                const subject = "Welcome to the app";
+                const message = `Hi, 
+                \n\n
+                Thanks for Signing Up.
+                `
+                await sendMail(user.email, subject, message)
+            })
+
+            return {success: true}
+
+        } catch (error) {
+            console.error("Error running email step", error.message)
+            return {success: false}
+        }
+    }
+)
